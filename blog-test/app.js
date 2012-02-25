3d24a39cb9aaa56f24ffbc320be816b57c526552
langmi <daywalker2000@gmx.net>
2012-02-25 23:00:28 +0100

    example to make node.js script executable

--- blog-test/app.js
+++ blog-test/app.js
@@ -1,23 +1,3 @@
-// dependencies
-var 
-    Fs = require('fs'),
-    Markdown = require('markdown'),
-    Plates = require('plates');
+#!/usr/bin/env node
+console.log("Hello World!");
 
-// provide template as globale variable
-var template;
-// use readFile synchronously
-template = Fs.readFileSync('templates/simple-html5-template.html', 'utf-8');
-
-// read markdown file with utf-8 encoding, this time asynchronously
-Fs.readFile('articles/hello-world.md', 'utf-8', function(err,data){
-    if(err) {
-        console.error("Could not open file: %s", err);
-        process.exit(1);
-    }
-    // parse with markdown
-    var mdOutput = Markdown.markdown.toHTML(data);
-    var data = { "content": mdOutput };
-    var output = Plates.bind(template, data);
-    console.log(output);
-});
---
+++ blog-test/archive/example-executable-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+// example for an executable javascript (node.js) script
+// first the environment, uses the env script, see http://en.wikipedia.org/wiki/Shebang_(Unix)
+// #!/usr/bin/env node has to be the first line
+// if not done, make the script executable with chmod u+x <script>
+// run it with ./<script> arg1 arg2 ...
+//
+// real simple hello world
+console.log("Hello World!");
